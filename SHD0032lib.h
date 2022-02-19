6a8145fdac53da2003525f168230ee53d805c009
Alexander Bykov <99946087+aLeXdEvElOpMeNt22@users.noreply.github.com>
2022-02-19 11:01:36 +0300

    Update SHD0032lib.h

--- SHD0032lib.h
+++ SHD0032lib.h
@@ -3,9 +3,9 @@
 #define SHD0032LIB_H_
 
 #include <avr/io.h>
-#include <util/delay.h>
 void Soft_SPI_Write(unsigned char data);
 void SHD0032_Write(unsigned char d1, unsigned char d2, unsigned char d3, unsigned char d4, unsigned char dot);
-void SHD0032_Show(unsigned char _min, unsigned char hour, unsigned char dot);
+void SHD0032_Show(unsigned char _min, unsigned char hour, unsigned char dot);
+
 void default_image(void);
 #endif
